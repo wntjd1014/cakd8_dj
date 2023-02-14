4c120c0d3ed939bad9b55079a608bf1d1f8a2b65
LeeJooseong <wntjd1014@gmail.com>
2023-02-14 11:38:34 +0900

    0214

--- homepage/views.py
+++ homepage/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # 이걸 클라이언트에게 보여주는거다.
 def landing(request):
+    recent_posts = Post.objects.order_by('-pk')[:3]
     return render(
         request,
-        'homepage/landing.html'
+        'homepage/landing.html',
+        {
+            'recent_posts': recent_posts, 
+        }
     )
 
 def about_me(request):
